145aac653cbb56fc7ce454d42fea7a042c992088
riskalaluna <riskalaluna@gmail.com>
2023-06-30 11:59:07 +0700

    This funcation searches the currentNode of the specified Node as well as the current Node of its parent

--- binaryseaechtree_176/binaryseaechtree_176.cpp
+++ binaryseaechtree_176/binaryseaechtree_176.cpp
@@ -56,4 +56,19 @@ public:
 	}
 
 
+	void search(string element, Node*& parent, Node*& currentNode)
+	{
+		//This funcation searches the currentNode of the specified Node as well as the current Node of its parent
+		currentNode = ROOT;
+		parent = NULL;
+		while ((currentNode != NULL) && (currentNode->info != element))
+		{
+			parent = currentNode;
+			if (element < currentNode->info)
+				currentNode = currentNode->leftchild;
+			else
+				currentNode = currentNode->rightchild;
+		}
+	}
+
 };
