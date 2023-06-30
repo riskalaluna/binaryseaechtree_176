57fcd06f0d16a4a81a15e5da1d6f09fb43244ea4
riskalaluna <riskalaluna@gmail.com>
2023-06-30 12:15:14 +0700

    case 1

--- binaryseaechtree_176/binaryseaechtree_176.cpp
+++ binaryseaechtree_176/binaryseaechtree_176.cpp
@@ -130,5 +130,21 @@ int main()
 		cout << "4. Perfrom postorder traversal" << endl;
 		cout << "5. Exit" << endl;
 		cout << "\nEnter your choice (1-5) : ";
+
+		char ch;
+		cin >> ch;
+		cout << endl;
+
+		switch (ch)
+		{
+		case '1':
+		{
+			cout << "Enter a word: ";
+			string word;
+			cin >> word;
+			obj.insert(word);
+			break;
+		}
+		}
 	}
 }
